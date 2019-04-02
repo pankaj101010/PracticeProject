3f06b963a7c87487ec8b0543fa19f4ffc34a69bc
pankaj101010 <pankajdevgade10@gmail.com>
2019-04-02 16:18:28 +0530

    new updates

--- src/com/practice/NumberAndAlphabateTriangle.java
+++ src/com/practice/NumberAndAlphabateTriangle.java
@@ -4,13 +4,14 @@ public class NumberAndAlphabateTriangle {
        
 	public static void main(String[] args) {
 		int x=1;
-	     char y='a';
+	    // char y='a';
 	       int z=1;
         for (int i = 4; i>=1; i--) {
 			for (int j = 0; j <=i-2; j++) {
 				System.out.print(" ");
 			}
 			if (x%2==0) {
+				char y='a';
 				for (int j = 1; j <=x; j++) {
 					System.out.print(y+" ");
 					y++;
